0655e498693f20199d2896e020fde92d26e5c15a
circius <circius@posteo.de>
2020-08-22 13:41:28 +0100

    reformatting EBNL descriptions so that 1) they're invalid 2) jsdoc can read them

--- src/todoDSL.js
+++ src/todoDSL.js
@@ -3,20 +3,24 @@
  * @description
  * consumes a list of tokens and parses them according to the 
  * following insanely simple grammar:
- * <expr> ::= <command> { <arg> }
- * <command> ::= :{ [a-zA-Z_] }
- * <arg> ::= { [^"]* }
+ * [ expr ] ::= [ command  ] [ arg ] 
+ * [ command  ] ::=  :[a-zA-Z_] 
+ * [ arg ] ::=  [^"]* 
 
  * Note that proper validation of output is TODO.
  * @param {Array} l
- * @return {(Array<String, Array<String>>)|Boolean} 
+ * @return {(Array[ String, Array<String>  ])|Boolean} 
  */
 function parseInstruction(l) {
   function validSyntaxP(command){
     return command[0] === ":";
   }
+
+  function atLeastOneElementP(l){
+    return l.length > 0
+  }
   
-  if (l.length === 0) return false;
+  if  (!atLeastOneElementP(l)) return false;
 
   const [command, ...arglist] = l
 
@@ -26,7 +30,7 @@ function parseInstruction(l) {
 /**
  * @description
  * Consumes a string representing a todoDSL expr and tokenizes it
- * according to the pattern <token><whitespace><token>.
+ * according to the pattern [ token ] [ whitespace ] [ token ].
  *
  * @param {String} s
  * @return {Array>String>} 
